27f80af8c3169a5e9711b3a66a3b6aef2976596f
Melvice Guimfack <melvice-junior.guimfack@polymtl.ca>
2024-11-12 14:57:39 -0500

    redirect issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
 import { Events } from "./pages/Event";
 import { AboutUs } from "./pages/AboutUs";
